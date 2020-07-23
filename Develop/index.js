91f858fc38cc0fdf796a6feca9c3fbd3dfb8e91e
GCaplan <gabriel.caplan@gmail.com>
2020-07-22 21:13:07 -0400

    added inquirer function and promise

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,47 @@
-// array of questions for user
-const questions = [
+// array of questions (might use spread into array?)
+const questions = [];
+// store the user data
+const inquirer = require("inquirer");
+// fs - for file working
+var fs = require("fs");
 
-];
-
+// run generate Mardown function========================
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// (*defined not called) grab that FS to turn string into file module maybe append?
+function writeToFile(fileName, data) {}
 
-// function to initialize program
+// function to initialize program (*kicking things off)
 function init() {
-
+  // prompt question for the user
+  // inquirer module
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your title?",
+        name: "title",
+      },
+    ])
+    .then(function (response) {
+      var fs = require("fs");
+      fs.writeFile("log.txt", JSON.stringify(response), function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Success!");
+      });
+    });
+  // Title
+  // Description
+  // Table of Contents
+  // Installation
+  // Usage
+  // License
+  // Contributing
+  // Tests
+  // Questions
+  // 1 badge
+  // emails -------------- finish the list
 }
 
 // function call to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,6 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  // we're dealing with template literal - a string - an object da da da
   return `# ${data.title}
 
 `;
